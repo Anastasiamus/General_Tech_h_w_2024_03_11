428e7cc9dd2f1af4afce2b10f6dca7ffd61a08c4
Anastasia Sobyanina <145497509+Anastasiamus@users.noreply.github.com>
2024-03-22 13:45:40 +0100

    Add files via upload

---
+++ General Tech h-w 2024_03_11.sql
@@ -0,0 +1,81 @@
+use homework;
+
+-- 1.Вывести имя, фамилию пользователей и даты их заказов(order_date).
+
+select 
+	t1.cust_first_name as first_name ,
+    t1.cust_last_name as last_name ,
+	t2.order_date 
+from customers t1
+join orders t2
+on t1.customer_id = t2.customer_id;
+
+
+-- 2.Вывести даты заказов продуктов и описание этих продуктов(product_description).
+
+select
+	t1.order_date,
+    t3.product_description
+from orders t1					
+join order_items t2
+on t1.order_id = t2.order_id 
+join product_information t3
+on 	t2.product_id = t3.product_id;
+   
+
+-- 3.Вывести имя, фамилию пользователей, даты заказов(orderdate), описание продуктов, которые они заказали и категории соответствующих продуктов (categoryname).
+
+select
+	t1.cust_first_name as first_name , 
+    t1.cust_last_name as last_name ,
+    t2.order_date ,
+    t3.product_description ,
+    t4.category_name
+    from customers t1
+    join orders t2
+    on t1.costumer_id = t2.costumer_id 
+    join product_information t3
+    on t1.costumer_id = t3.product_id
+    join categories_tab t4
+    on t3.category_id =t4.category_id
+    
+
+
+-- 4.Вывести названия(productname), описания категорий(categorydescription) 
+-- и количества(quantity) тех продуктов, у которых минимальная стоимость (min_price) больше 300.
+
+select t1.product_name, t2.category_description, t3.quantity, t1.MIN_PRICE
+from product_information t1
+join categories_tab t2 on t1.category_id=t2.category_id
+join order_items t3  on t1.product_id= t3.product_id
+where t1.MIN_PRICE> 300;
+
+
+-- 5.Вывести имя, фамилию покупателей, которые купили принтеры.
+
+select t1.cust_first_name, t1.cust_last_name, t5.category_description
+from customers t1
+join orders t2 on t1.customer_id=t2.customer_id
+join order_items t3 on t2.order_id=t3.order_id
+join product_information t4 on t3.product_id=t4.product_id
+join categories_tab t5 on t4.category_id= t5.category_id
+where category_description like 'printers';
+
+-- 6.Вывести имя, фамилию всех женатых мужчин покупателей, которые покупали продукты со стоимостью (list_price) больше 550.
+
+select t1.cust_first_name, t1.cust_last_name 
+from customers t1 
+join orders t2 on t1.customer_id=t2.customer_id
+join order_items t3 on t2.order_id=t3.order_id
+join product_information t4 on t3.product_id=t4.product_id
+where t1.marital_status = 'Married'
+and t1.gender = 'M' 
+and t4.list_price > 550;
+
+-- 7.Вывести названия(product_name) тех продуктов, у которых стоимость больше, чем у продукта 'LaserPro 600/6/BW’.
+
+select t1.product_name
+from product_information t1
+join product_information t2 on t1.list_price > t2.list_price
+where t2.product_name = 'LaserPro 600/6/BW';
+
